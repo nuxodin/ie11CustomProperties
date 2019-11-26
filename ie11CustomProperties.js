9077a005db337720c1060d2466e897f726b349a7
Tobias Buschor <tobias.buschor@shwups.ch>
2019-11-26 14:18:39 +0100

    transform relative urls

--- ie11CustomProperties.js
+++ ie11CustomProperties.js
@@ -118,6 +118,7 @@
 		fetchCss(el.href, function (css) {
 			var newCss = rewriteCss(css);
 			if (css === newCss) return;
+			newCss = relToAbs(el.href, newCss);
 			el.disabled = true;
 			var style = document.createElement('style');
 			el.parentNode.insertBefore(style, el);
@@ -138,6 +139,15 @@
 		if (found.getters) addGetterElement(el, found.getters, '%styleAttr');
 		if (found.setters) addSetterElement(el, found.setters);
 	}});
+
+	function relToAbs(base, css) {
+		return css.replace(/url\(([^)]+)\)/, function($0, $1){
+			$1 = $1.trim().replace(/(^['"]|['"]$)/g,'');
+			if ($1.match(/^([a-z]+:|\/)/)) return $0;
+			base = base.replace(/\?.*/,'');
+			return 'url('+ base + './../' + $1 +')';
+		});
+	}
 
 	// ie has a bug, where unknown properties at pseudo-selectors are computed at the element
 	// #el::after { -content:'x'; } => getComputedStyle(el)['-content'] == 'x'
