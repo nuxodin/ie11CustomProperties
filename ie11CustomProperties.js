9c57cea4ba37e91aeb885652878322275b621a72
Tobias Buschor <tobias.buschor@shwups.ch>
2019-12-19 19:35:58 +0100

    add tests

--- ie11CustomProperties.js
+++ ie11CustomProperties.js
@@ -1,4 +1,4 @@
-/*! ie11CustomProperties.js v2.6.0 | MIT License | https://git.io/fjXMN */
+/*! ie11CustomProperties.js v2.7.0 | MIT License | https://git.io/fjXMN */
 // c1.onElement helper
 !function () {
     'use strict';
